52501be35a952e63e5eef8466612c1a32303ea2d
tabet-anthony <anthony_tabet01@hotmail.com>
2024-05-07 19:12:36 +0300

    the training for my local models

---
+++ Road_Pothole_detection/road_Pothole_detection_model_train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import tensorflow as tf
+import torch
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO("YOLOv8x.yaml")  # Specify model scale
+    model.to("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Train the model
+    model.train(
+        data=r'C:\Users\Antho\OneDrive\Desktop\Industrial AI Final Project\Road_Pothole_detection\pothole_dataset_v8\pothole_dataset_v8\pothole.yaml',
+        epochs=100, imgsz=640, name="trainFolderFromScratch", batch=32, workers=2)
+
